397f83069c19e42e7874bc343a0b243e2dde23a4
Icarus <Icarus_Alpha@outlook.com>
2023-11-13 18:40:07 +0800

    辟邪剑法

--- src/main/java/com/atguigu/git/DuGuJiuJian.java
+++ src/main/java/com/atguigu/git/DuGuJiuJian.java
@@ -7,6 +7,8 @@ package com.atguigu.git;
 public class DuGuJiuJian {
     public static void main(String[] args) {
         System.out.println("独孤九剑");
+
+        System.out.println("独孤九剑乃天下第一剑法，剑法已经到了炉火纯青的境界");
     }
 }
 
